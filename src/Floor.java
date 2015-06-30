e7d879d68b3db8d89042c95fc84b3a5277636cec
nandesh <nnnaik92@gmail.com>
2015-06-30 16:10:15 -0700

    floor funtions added

--- src/Floor.java
+++ src/Floor.java
@@ -1,5 +1,22 @@
 
 public class Floor{
-
+    private int currentFloor ; 
 	Door  door  = new Door();
+	 
+	public Floor(){
+		currentFloor = 0  ;
+		
+	}
+	
+	
+	public Integer getCurrentFloorNumber() {
+		return currentFloor;
+	}
+
+	// setter function for CurrentFloorNumber
+	public void setCurrentFloorNumber(Integer currentFloorNumber) {
+		this.currentFloor = currentFloorNumber;
+	} 
+	
+	
 }
