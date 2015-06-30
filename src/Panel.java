92e60629938bd6888c9e5637320e0864c609fcf8
amitgude <gudeamit148@gmail.com>
2015-06-30 16:08:58 -0700

    Added display and panelDisplay methods

--- src/Panel.java
+++ src/Panel.java
@@ -1,5 +1,21 @@
+import java.util.Scanner;
+
 
 public class Panel {
 
 	Buttons buttons = new Buttons();
+	Scanner scan = new Scanner(System.in);
+	
+	void display(){
+		System.out.println("Select a Floor :");
+		System.out.println("0 1 2 3 4 5 ");
+		System.out.println("6 7 8 9 10");
+	}
+	
+	
+	void panelDisplay(){
+		System.out.println("Enter a floor number:");
+		int option = scan.nextInt();
+		buttons.click(option);
+	}
 }
