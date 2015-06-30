6e9caa7f4bbb3220df8e7f8ea48feba8cb9cb3fa
amitgude <gudeamit148@gmail.com>
2015-06-30 16:10:57 -0700

    Added click method

--- src/Buttons.java
+++ src/Buttons.java
@@ -1,4 +1,18 @@
 
 public class Buttons {
 
+	private Integer value;
+
+	public Integer getValue() {
+		return value;
+	}
+
+	public void setValue(Integer value) {
+		this.value = value;
+	}
+	
+	void click(Integer option){
+		setValue(option);
+	}
+	
 }
