104ccba3397286cdfd83ee25ae1f4fbdc5efb055
amitgude <gudeamit148@gmail.com>
2015-07-02 15:32:58 -0700

    version 3

--- src/Main.java
+++ src/Main.java
@@ -3,36 +3,42 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		//cuurrent floor is floor of user 
+		//current floor is floor of user 
 		int currentFloor;
 		Elevator elevator = new Elevator();
-
+		
+		String eleStatus;
 		// scenario 1 
-		currentFloor  =  3 ; 
-		elevator.callElevator(currentFloor);
-		elevator.changeFloor();
 
 
-		// scenario 2 
 		currentFloor  =  3 ;
-		elevator.setFloor(10);
-		elevator.callElevator(currentFloor);
-		elevator.changeFloor();
+		System.out.println("You are currently on floor : " +currentFloor);
+		eleStatus =elevator.callElevator(currentFloor);
+		System.out.println("Lift is going "+ eleStatus);
+		eleStatus =elevator.changeFloor();
+		System.out.println("Lift is going "+ eleStatus);
+		System.out.println("Now you are on floor : " +elevator.getElevatorFloor());
 
-
-		// scenario 3
-
-		currentFloor  =  5 ;
-		elevator.setFloor(5);
-		elevator.callElevator(currentFloor);
-		elevator.changeFloor();
-
-
-		// scenario 4
-		currentFloor  =  5 ;
-		elevator.setFloor(0);
-		elevator.callElevator(currentFloor);
-		elevator.changeFloor();
+//		// scenario 2 
+//		currentFloor  =  3 ;
+//		elevator.setFloor(10);
+//		elevator.callElevator(currentFloor);
+//		elevator.changeFloor();
+//
+//
+//		// scenario 3
+//
+//		currentFloor  =  5 ;
+//		elevator.setFloor(5);
+//		elevator.callElevator(currentFloor);
+//		elevator.changeFloor();
+//
+//
+//		// scenario 4
+//		currentFloor  =  5 ;
+//		elevator.setFloor(0);
+//		elevator.callElevator(currentFloor);
+//		elevator.changeFloor();
 
 
 
