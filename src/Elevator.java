b414be1b45a483a171533f769e7fd422f3163bb6
nandesh <nnnaik92@gmail.com>
2015-07-02 15:57:36 -0700

    version 4

---
+++ src/Elevator.java
@@ -0,0 +1,84 @@
+
+public class Elevator {
+
+	Panel panel = new Panel();
+	Door  door  = new Door();
+	Floor floor = new Floor();
+	
+	String elevStatus;
+	
+	//elevator movement
+	
+	
+	public void setFloor(int floorno){
+		floor.setCurrentFloorNumber(floorno);
+		
+	}
+	
+	
+	public String elevMove(int currentFloor){
+		 
+		if (floor.getCurrentFloorNumber() > currentFloor){
+			elevStatus = "down";
+		}
+		else if (floor.getCurrentFloorNumber() < currentFloor){
+			elevStatus = "up";
+		}
+		floor.setCurrentFloorNumber(currentFloor);
+		
+		floor.openFloorDoor(); // opening floor door 
+		door.opendoor();        // opening elevator door 
+		
+		floor.closeFloorDoor();
+		door.closedoor();
+		
+		return elevStatus;
+	}
+	
+	public int getElevatorFloor(){
+		return floor.getCurrentFloorNumber();
+	}
+	
+	//method to call elevator
+
+	
+	public String  callElevator(int currentFloor , String opt ){
+		floor.selectOption(opt);
+
+		String option = floor.getFloorOption();
+		
+		if (option.equalsIgnoreCase("up")){
+			elevStatus = elevMove(currentFloor);
+			
+		}
+		else if (option.equalsIgnoreCase("down")){
+			elevStatus = elevMove(currentFloor);
+		}
+			
+		return elevStatus;
+	}
+	
+	//method to change floor
+	public String changeFloor(int floorno){
+	    panel.setSelectedFloor(floorno);
+		if ( getElevatorFloor() > panel.getSelectedFloor() ){
+			elevStatus = "down";
+		}
+		else if ( getElevatorFloor() < panel.getSelectedFloor()){
+			elevStatus = "up";
+		}
+				
+		floor.setCurrentFloorNumber(panel.getSelectedFloor());
+		
+		floor.openFloorDoor(); // opening floor door 
+		door.opendoor();        // opening elevator door 
+		
+		floor.closeFloorDoor();
+		door.closedoor();
+		
+		return elevStatus;
+	}
+	
+	
+	
+}
